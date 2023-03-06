551338a4d9213abbedc257dff5914e6d43d85549
mas-wig <mas_wig@protonmail.ch>
2023-03-06 23:35:37 +0700

    add some shit

--- lua/plugins/lsp/config/lsp-server.lua
+++ lua/plugins/lsp/config/lsp-server.lua
@@ -1,15 +1,126 @@
+local dirname = function(pathname)
+	local path_sep = "/"
+	local strip_dir_pat = path_sep .. "([^" .. path_sep .. "]+)$"
+	local strip_sep_pat = path_sep .. "$"
+	if not pathname or #pathname == 0 then
+		return
+	end
+	local result = pathname:gsub(strip_sep_pat, ""):gsub(strip_dir_pat, "")
+	if #result == 0 then
+		return "/"
+	end
+	return result
+end
+
 return {
-    lua_ls = {
-        cmd = {"lua-language-server"},
-        settings = {
-            Lua = {
-                workspace = {
-                    checkThirdParty = false
-                },
-                completion = {
-                    callSnippet = "Replace"
-                }
-            }
-        }
-    }
+	lua_ls = {
+		cmd = { "lua-language-server" },
+		settings = {
+			Lua = {
+				workspace = {
+					checkThirdParty = false,
+				},
+				completion = {
+					callSnippet = "Replace",
+				},
+			},
+		},
+	},
+
+	phpactor = {
+		cmd = { "phpactor", "language-server" },
+		filetypes = { "php" },
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+	},
+
+	pyright = {
+		on_init = function(client)
+			require("navigator.lspclient.python").on_init(client)
+		end,
+		cmd = { "pyright-langserver", "--stdio" },
+		filetypes = { "python" },
+		flags = { allow_incremental_sync = true, debounce_text_changes = 500 },
+		settings = {
+			python = {
+				formatting = { provider = "black" },
+				analysis = {
+					autoSearchPaths = true,
+					useLibraryCodeForTypes = true,
+					diagnosticMode = "workspace",
+				},
+			},
+		},
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+	},
+
+	html = {
+		cmd = { "vscode-html-language-server", "--stdio" },
+		filetypes = { "html" },
+		init_options = {
+			configurationSection = { "html", "css", "javascript" },
+			embeddedLanguages = {
+				css = true,
+				javascript = true,
+			},
+			provideFormatter = true,
+		},
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+	},
+
+	cssls = {
+		cmd = { "vscode-css-language-server", "--stdio" },
+		filetypes = { "css", "scss", "less" },
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+		settings = {
+			css = {
+				validate = true,
+			},
+			less = {
+				validate = true,
+			},
+			scss = {
+				validate = true,
+			},
+		},
+	},
+
+	sqls = {
+		cmd = { "sqls" },
+		filetypes = { "sql", "mysql" },
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+	},
+	clangd = {
+		flags = { allow_incremental_sync = true, debounce_text_changes = 500 },
+		cmd = {
+			"clangd",
+			"--background-index",
+			"--suggest-missing-includes",
+			"--clang-tidy",
+			"--header-insertion=iwyu",
+			"--enable-config",
+			"--offset-encoding=utf-16",
+			"--clang-tidy-checks=-*,llvm-*,clang-analyzer-*",
+			"--cross-file-rename",
+		},
+		filetypes = { "c", "cpp", "objc", "objcpp" },
+		root_dir = function(fname)
+			return require("lspconfig").util.root_pattern(".git")(fname) or dirname(fname)
+		end,
+		single_file_support = true,
+	},
 }
--- lua/setup/autocmds.lua
+++ lua/setup/autocmds.lua
@@ -1,7 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 local function augroup(name)
-    return vim.api.nvim_create_augroup(name, {clear = true})
+	return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
 -- fold
@@ -14,132 +14,147 @@ vim.cmd([[
     ]])
 
 autocmd("BufWinEnter", {
-    group = augroup("uncomment_newline"),
-    pattern = "*.*",
-    command = "set formatoptions-=cro",
+	group = augroup("uncomment_newline"),
+	pattern = "*.*",
+	command = "set formatoptions-=cro",
 })
 
 -- Reload file
-autocmd({"FocusGained", "TermClose", "TermLeave"}, {
-    group = augroup("checktime"),
-    command = "checktime",
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = augroup("checktime"),
+	command = "checktime",
 })
 
 -- Highlight on yank
 autocmd("TextYankPost", {
-    group = augroup("highlight_yank"),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- resize splits if window got resized
-autocmd({"VimResized"}, {
-    group = augroup("resize_splits"),
-    callback = function()
-        vim.cmd("tabdo wincmd =")
-    end,
+autocmd({ "VimResized" }, {
+	group = augroup("resize_splits"),
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
 })
 
 -- go to last loc when opening a buffer
 autocmd("BufReadPost", {
-    group = augroup("last_loc"),
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+	group = augroup("last_loc"),
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
 
 -- close some filetypes with <q>
 autocmd("FileType", {
-    group = augroup("close_with_q"),
-    pattern = {
-        "qf",
-        "help",
-        "man",
-        "notify",
-        "lspinfo",
-        "spectre_panel",
-        "startuptime",
-        "tsplayground",
-        "PlenaryTestPopup",
-    },
-    callback = function(event)
-        vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", "<cmd>close<cr>", {buffer = event.buf, silent = true})
-    end,
+	group = augroup("close_with_q"),
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"notify",
+		"lspinfo",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"PlenaryTestPopup",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
 
 -- wrap and check for spell in text filetypes
 autocmd("FileType", {
-    group = augroup("wrap_spell"),
-    pattern = {"gitcommit", "markdown"},
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.spell = true
-    end,
+	group = augroup("wrap_spell"),
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 -- exclude qf buffer
 autocmd("FileType", {
-    pattern = "qf",
-    callback = function()
-        vim.opt_local.buflisted = false
-    end,
+	pattern = "qf",
+	callback = function()
+		vim.opt_local.buflisted = false
+	end,
 })
 
 -- Format on save
 autocmd("BufWritePost", {
-    pattern = "*",
-    command = "FormatWrite",
-    group = augroup("FormatAutogroup"),
+	pattern = "*",
+	command = "FormatWrite",
+	group = augroup("FormatAutogroup"),
 })
 
 -- refresh lsp-progress
 autocmd("User", {
-    pattern = "LspProgressStatusUpdated",
-    command = "lua require('lualine').refresh()",
-    group = augroup("lualine_augroup"),
+	pattern = "LspProgressStatusUpdated",
+	command = "lua require('lualine').refresh()",
+	group = augroup("lualine_augroup"),
 })
 
 local exclude_ft = {
-    "lazy",
-    "NvimTree",
-    "toggleterm",
-    "alpha",
-    "crunner_main",
-    "terminal",
-    "help",
-    "dashboard",
-    "Trouble",
-    "octo",
-    "mason",
-    "dbui",
-    "TelescopePromt",
-    "chatgpt",
-    "Telescope",
+	"lazy",
+	"NvimTree",
+	"toggleterm",
+	"alpha",
+	"crunner_main",
+	"terminal",
+	"help",
+	"dashboard",
+	"Trouble",
+	"octo",
+	"mason",
+	"dbui",
+	"TelescopePromt",
+	"chatgpt",
+	"Telescope",
 }
 
 autocmd("FileType", {
-    pattern = exclude_ft,
-    callback = function()
-        if vim.bo.ft == exclude_ft then
-            vim.opt.laststatus = 0
-            require("indent_blankline").setup({
-                filetype_exclude = exclude_ft,
-            })
-        end
-        vim.b.miniindentscope_disable = true
-    end,
+	pattern = exclude_ft,
+	callback = function()
+		if vim.bo.ft == exclude_ft then
+			vim.opt.laststatus = 0
+			require("indent_blankline").setup({
+				filetype_exclude = exclude_ft,
+			})
+		end
+		vim.b.miniindentscope_disable = true
+	end,
 })
 
 -- uncomment new lines
 autocmd("BufEnter", {
-    pattern = "*",
-    callback = function()
-        vim.opt.formatoptions = vim.opt.formatoptions - {"c", "r", "o"}
-    end,
+	pattern = "*",
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+	end,
+})
+
+autocmd("FileType", {
+	group = augroup("off_documentation"),
+	pattern = {
+		"sql",
+		"mysql",
+	},
+	callback = function()
+		require("cmp").setup({
+			window = {
+				documentation = false,
+			},
+		})
+	end,
 })

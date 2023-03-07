83f948c684a1dc0e089102caf45dd3f16ac7e368
mas-wig <mas_wig@protonmail.ch>
2023-03-08 02:34:52 +0700

    add some shit

--- lua/plugins/coding/nvim-treesitter.lua
+++ lua/plugins/coding/nvim-treesitter.lua
@@ -1,143 +1,94 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	version = false,
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	init = function()
-		require("setup.plugins").on_file_open("nvim-treesitter")
-	end,
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
-	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-		{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-		{ "windwp/nvim-ts-autotag", lazy = true, config = true },
-		{ "RRethy/nvim-treesitter-endwise", lazy = true },
-		{ "HiPhish/nvim-ts-rainbow2", lazy = true },
-		{ "nvim-treesitter/nvim-treesitter-context", lazy = true, config = true },
-		{ "nvim-treesitter/playground", lazy = true },
-		{
-			"windwp/nvim-autopairs",
-			lazy = true,
-			opts = {
-				close_triple_quotes = true,
-				check_ts = true,
-				fast_wrap = {
-					map = "<c-e>",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		init = function()
+			require("setup.plugins").on_file_open("nvim-treesitter")
+		end,
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
+		dependencies = {
+			{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+			{ "windwp/nvim-ts-autotag", lazy = true, config = true },
+			{ "RRethy/nvim-treesitter-endwise", lazy = true },
+			{ "HiPhish/nvim-ts-rainbow2", lazy = true },
+			{ "nvim-treesitter/nvim-treesitter-context", lazy = true, config = true },
+			{ "nvim-treesitter/playground", lazy = true },
+			{
+				"windwp/nvim-autopairs",
+				lazy = true,
+				opts = {
+					close_triple_quotes = true,
+					check_ts = true,
+					fast_wrap = {
+						map = "<c-e>",
+					},
+				},
+			},
+			{
+				"abecodes/tabout.nvim",
+				lazy = true,
+				opts = {
+					tabkey = "<Tab>",
+					backwards_tabkey = "<S-Tab>",
+					completion = true,
 				},
 			},
 		},
-		{
-			"abecodes/tabout.nvim",
-			lazy = true,
-			opts = {
-				tabkey = "<Tab>",
-				backwards_tabkey = "<S-Tab>",
-				completion = true,
-			},
-		},
-	},
-	config = function()
-		return require("nvim-treesitter.configs").setup({
-			ensure_installed = "all",
-			sync_install = false,
-			ignore_install = { "phpdoc" },
-			highlight = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = "<nop>",
-					node_decremental = "<bs>",
-				},
-			},
-			textsubjects = { enable = true, keymaps = { [","] = "textsubjects-smart" } },
-			indent = { enable = true },
-			autotag = { enable = true },
-			endwise = { enable = true },
-			context_commentstring = { enable = true },
-			rainbow = {
-				enable = true,
-				disable = { "jsx", "cpp" },
-				query = "rainbow-parens",
-				strategy = require("ts-rainbow.strategy.global"),
-			},
-			query_linter = {
-				enable = true,
-				use_virtual_text = true,
-				lint_events = { "BufWrite", "CursorHold" },
-			},
-
-			textobjects = {
-				-- syntax-aware textobjects
-				enable = true,
-				lsp_interop = {
+		config = function()
+			return require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				sync_install = false,
+				ignore_install = { "phpdoc" },
+				highlight = {
 					enable = true,
-					peek_definition_code = {
-						["DF"] = "@function.outer",
-						["Df"] = "@class.outer",
-					},
 				},
-				keymaps = {
-					["iL"] = {
-						-- you can define your own textobjects directly here
-						go = "(function_definition) @function",
-					},
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["aC"] = "@class.outer",
-					["iC"] = "@class.inner",
-					["ac"] = "@conditional.outer",
-					["ic"] = "@conditional.inner",
-					["ae"] = "@block.outer",
-					["ie"] = "@block.inner",
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-					["is"] = "@statement.inner",
-					["as"] = "@statement.outer",
-					["ad"] = "@comment.outer",
-					["am"] = "@call.outer",
-					["im"] = "@call.inner",
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
-				},
-				select = {
+				incremental_selection = {
 					enable = true,
 					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["iF"] = {
-							python = "(function_definition) @function",
-							cpp = "(function_definition) @function",
-							c = "(function_definition) @function",
-							java = "(method_declaration) @function",
-							go = "(method_declaration) @function",
-						},
+						init_selection = "gnn",
+						node_incremental = "grn",
+						scope_incremental = "grc",
+						node_decremental = "grm",
 					},
 				},
-			},
-		})
-	end,
+				textsubjects = { enable = true, keymaps = { [","] = "textsubjects-smart" } },
+				indent = { enable = true },
+				autotag = { enable = true },
+				endwise = { enable = true },
+				context_commentstring = { enable = true },
+				rainbow = {
+					enable = true,
+					disable = { "jsx", "cpp" },
+					query = "rainbow-parens",
+					strategy = require("ts-rainbow.strategy.global"),
+				},
+				query_linter = {
+					enable = true,
+					use_virtual_text = true,
+					lint_events = { "BufWrite", "CursorHold" },
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		init = function()
+			local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+			local enabled = false
+			if opts.textobjects then
+				for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+					if opts.textobjects[mod] and opts.textobjects[mod].enable then
+						enabled = true
+						break
+					end
+				end
+			end
+			if not enabled then
+				require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+			end
+		end,
+	},
 }
---
+++ lua/plugins/editor/nvim-spectre.lua
@@ -0,0 +1,20 @@
+return {
+	"windwp/nvim-spectre",
+	keys = {
+		{
+			"<leader>sr",
+			function()
+				require("spectre").open()
+			end,
+			desc = "Replace in files (Spectre)",
+		},
+	},
+	config = function()
+		return require("spectre").setup({
+			live_update = true,
+			line_sep_start = "🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲",
+			result_padding = " 🧩 ",
+			line_sep = "🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲",
+		})
+	end,
+}

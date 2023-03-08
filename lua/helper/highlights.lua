4b2194d590d694567715d3e22312e9c88d5ba53e
mas-wig <mas_wig@protonmail.ch>
2023-03-08 14:54:28 +0700

    add some shit

--- lua/helper/highlights.lua
+++ lua/helper/highlights.lua
@@ -1,15 +1,23 @@
 return {
 	{ "LineNr", { fg = "#ffffff" } },
+
 	{ "TelescopeBorder", { fg = "#c4ff4d", bg = "none" } },
 	{ "TelescopePromptTitle", { bg = "#ff4d4d", fg = "#000000" } },
 	{ "TelescopePreviewTitle", { bg = "#79ff4d", fg = "#000000" } },
 	{ "TelescopeResultsTitle", { bg = "#66ffcc", fg = "#000000" } },
+
 	{ "NvimTreeIndentMarker", { fg = "#1affd1", bg = "none" } },
+	{ "NvimTreeWinSeparator", { fg = "#ffff4d", bg = "none" } },
+	{ "NvimTreeFolderIcon", { fg = "#e6b800", bg = "none" } },
+
 	{ "WinSeparator", { fg = "#ffff4d", bg = "none" } },
+
 	{ "WhichKey", { fg = "#ff00ff", bg = "none" } },
 	{ "WhichKeyBorder", { fg = "#ace600", bg = "none" } },
+
 	{ "FloatBorder", { fg = "#e60073", bg = "none" } },
 	{ "WildMenu", { fg = "#bf40bf", bg = "none" } },
+
 	{ "NoiceCmdlinePrompt", fg = "none", bg = "#bfff80" },
 	{ "NoiceCmdlinePopupBorder", { fg = "#b84dff", bg = "none" } },
 
